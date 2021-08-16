573adabaaf2866581fd529aa6484355cd2022384
mkokosinski <mkokosinski91@gmail.com>
2021-08-16 11:11:10 +0200

    fixed tailwind, added missing types

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import React from "react";
-import { LandingPage } from "./Components/landingPage/LandingPage";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Routing } from "./Components/landingPage/routing/Routing";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import { LandingPage } from './Components/landingPage/LandingPage';
+import { Routing } from './Components/landingPage/routing/Routing';
+
+import './index.css';
 
 function App() {
   return (
     <Router>
+      <div className='bg-blue-300'>Test</div>
       <Routing />
     </Router>
   );
