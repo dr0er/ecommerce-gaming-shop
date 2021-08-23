a41e4d768ebce771e22d847fc1b1935f23349955
dr0er <ernestxxx123@gmail.com>
2021-08-23 11:29:35 +0200

    some changes

--- Frontend/src/Components/landingPage/LandingPage.tsx
+++ Frontend/src/Components/landingPage/LandingPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./LandingPage.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import "./LandingPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 interface IProps {}
@@ -10,26 +10,20 @@ interface IProps {}
 
 export const LandingPage: React.FC<IProps> = (props) => {
   return (
-    <div className="landingPage_Container" >
-
-
-    <div className="center_Text">
-        <p className="stragan_Text">Stragan.com</p>
+    <div className="landingPage_Container">
+      <div className="center_Text">
+        <p className="stragan_Text text-red-500">Stragan.com</p>
         <div className="center_Text_Info">
-            Next level of gaming {" "} 
-            <span className="shopping_Blue" >
-              shopping
-            </span> 
+          Next level of gaming <span className="shopping_Blue">shopping</span>
         </div>
-    </div>
+      </div>
 
-
-        <div className="button_GetStarted">
-          <Link to='/'>
-            Get started
-              <FontAwesomeIcon icon={faChevronRight}/>
-          </Link>
-        </div>
+      <div className="button_GetStarted">
+        <Link to="/">
+          Get started
+          <FontAwesomeIcon icon={faChevronRight} />
+        </Link>
+      </div>
     </div>
   );
 };
