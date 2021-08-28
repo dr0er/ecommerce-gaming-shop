8d73cc82d067066e320027e29c717dbecc96a8b2
MaksimMarynau <oliverknightbel@gmail.com>
2021-08-28 19:02:54 +0200

    urls

--- Backend/backend/urls.py
+++ Backend/backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:pk>/", views.ProductDetailView.as_view(),
+    path("product/<int:pk>/", views.ProductDetailView.as_view(),
         name='product_detail'
     ),
 ]
