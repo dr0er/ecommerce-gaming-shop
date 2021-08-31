5e2d62f1a426b2150788d160fe462bf8da7c7152
Sebastian-ctr <75362950+Sebastian-ctr@users.noreply.github.com>
2021-08-31 13:20:19 +0200

    add ModelVeiwSet to views for Products

--- Backend/backend/views.py
+++ Backend/backend/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import  ModelViewSet
+
+from .models import *
+from .serializers import *
+
+
+class ProductViewSet(ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
