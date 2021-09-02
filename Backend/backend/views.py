a01c6b6a82af8ab088e4ec3f060a08acd1572074
MaksimMarynau <oliverknightbel@gmail.com>
2021-09-02 18:44:52 +0200

    Create server logic for listing user orders EH-94

--- Backend/backend/views.py
+++ Backend/backend/views.py
@@ -8,7 +8,6 @@ from rest_framework.mixins import (
     DestroyModelMixin
 )
 
-
 from .models import (
     Address,
     Order,
@@ -44,3 +43,16 @@ class OrderItemCreateViewset(
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
     permission_classes = [IsAuthenticated]
+
+
+class OrderListViewset(
+                    ListModelMixin,
+                    GenericViewSet):
+    queryset = Order.objects.filter()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Order.objects.filter(user=user).order_by('-createdAt')
+        return queryset
