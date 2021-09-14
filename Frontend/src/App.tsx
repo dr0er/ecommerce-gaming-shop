b40ec28f29fa218093e3a4cc265b9d046d3425b3
mkokosinski <mkokosinski91@gmail.com>
2021-09-14 18:25:50 +0200

    test change

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Routing } from './routing/Routing'
 function App() {
+  //test git
   return (
     <div className="bg-background-grey">
       <Router>
