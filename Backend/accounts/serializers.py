bc95540e746fe599395132b7ad4f47a92146b1f6
dawid <dee2ess@gmail.com>
2021-09-14 11:07:57 +0200

    Fixed backend

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -1,73 +1,46 @@
-from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib import auth
-from rest_framework_simplejwt.tokens import RefreshToken
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.mixins import (
+    ListModelMixin,
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin
+)
+from django.contrib.auth import get_user_model
 from .utils import USER_MODEL
-from .models import CustomUser
-from backend.serializers import AddressSerializer
+from .serializers import (
+    RegistrationSerializer,
+    UserSerializer,
+)
 
 
-class RegistrationSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(
-        style={'input_type': 'password'},
-        write_only=True,
-        max_length=16
-    )
+class RegistrationViewset(
+                    CreateModelMixin,
+                    GenericViewSet):
+    queryset = USER_MODEL.objects.all()
+    serializer_class = RegistrationSerializer
 
-    class Meta:
-        model = USER_MODEL
-        fields = ['username', 'email', 'password', 'password2']
-        extra_kwargs = {
-            'password': {'write_only': True, 'max_length': 16,
-                         'style': {'input_type': 'password'}
-                         }
-        }
-
-    def save(self):
-        user = USER_MODEL(
-            username=self.validated_data['username'],
-            email=self.validated_data['email'],
-        )
-
-        password = self.validated_data['password']
-        password2 = self.validated_data['password2']
-
-        if password != password2:
-            raise serializers.ValidationError({'password': 'Passwords must match.'})
-
-        user.set_password(password)
-        user.is_active = True
-        user.save()
-        return user
+    def create(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            serializer = RegistrationSerializer(data=request.data)
+            data = {}
+            if serializer.is_valid():
+                user = serializer.save()
+                data['response'] = 'Successfully registered a new user!'
+                data['email'] = user.email
+                data['username'] = user.username
+            else:
+                data = serializer.errors
+            return Response(data)
 
 
-class UserSerializer(serializers.ModelSerializer):
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    address = serializers.SerializerMethodField()
+class UserViewset(ReadOnlyModelViewSet, UpdateModelMixin):
 
-    class Meta:
-        model = USER_MODEL
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active']
-        fields = ['id','username','email','first_name','last_name', 'date_of_birth', 'info', 'address']
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated,]
 
-    def get_address(self, obj):
-        try:
-            address = AddressSerializer(obj.address, many=False).data
-        except:
-            address = False
-        return address
-
-
-class UserSerializerWithToken(UserSerializer):
-    token = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = USER_MODEL
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_of_birth', 'info', 'address', 'token']
-
-    def get_token(self, obj):
-        token = RefreshToken.for_user(obj)
-        return str(token.access_token)
-
-
+    def get_queryset(self):
+        return get_user_model().objects.filter(pk=self.request.user.id)
