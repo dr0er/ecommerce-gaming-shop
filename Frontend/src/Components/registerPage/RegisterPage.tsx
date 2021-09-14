3ffcc5d7b7d0669cebc96b9ff5e450953cc906bc
godspeed7 <pawelpikus7@gmail.com>
2021-09-14 20:42:29 +0200

    Add register page

---
+++ Frontend/src/Components/registerPage/RegisterPage.tsx
@@ -0,0 +1,24 @@
+import RegisterForm from '../registerPage/RegisterForm'
+import { Link } from 'react-router-dom'
+import BackToMain from '../registerPage/BackToMain'
+import { Routes } from '../../Utilities/routes'
+
+const RegisterPage = () => {
+  return (
+    <div className="h-screen text-grey-light py-6 flex flex-col gap-6 items-center  max-w-md px-6 md: mx-auto">
+      <BackToMain />
+      <h1 className="font-bold text-3xl text-center ">Register to Stragan</h1>
+      <RegisterForm />
+      <p className="flex gap-2 opacity-70 mx-auto">
+        Already have an account?
+        <Link
+          to={Routes.login}
+          className="text-blue-500 opacity-100 cursor-pointer underline font-bold">
+          Login!
+        </Link>
+      </p>
+    </div>
+  )
+}
+
+export default RegisterPage
