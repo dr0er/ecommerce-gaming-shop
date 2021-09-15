2376efeb6474d573ba6990a92592f5d0bb35df5b
dr0er <ernestxxx123@gmail.com>
2021-09-15 20:40:29 +0200

    Add changes to navbar and menu

--- Frontend/src/Components/Navbar/Menu.tsx
+++ Frontend/src/Components/Navbar/Menu.tsx
@@ -2,23 +2,33 @@ import {faTimes} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import * as React from "react";
 import MenuItem from "./MenuItem";
+import {useState} from "react";
 
 export interface MenuProps {
     isOpened: boolean;
 }
 
 const Menu = ({isOpened}: MenuProps) => {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleToggleMenu = () => {
+        setIsExpanded((prev) => !prev);
+
+    };
+
     const baseClasses = `h-full w-8/12 flex flex-col 
     bg-background-grey absolute top-0 left-0 transform border-8 
     border-red-500 px-8 text-grey-light text-xl transition-transform duration-250`
 
     const classNames = isOpened ? baseClasses : baseClasses + " -translate-x-full"
+
     return (
         <div
             className={classNames}
         >
             <div className={"flex gap-6 items-center"}>
-                <FontAwesomeIcon icon={faTimes} />
+                <FontAwesomeIcon onClick={handleToggleMenu} icon={faTimes} />
                 Menu
             </div>
             <div className={"opacity-30 py-2"}>
