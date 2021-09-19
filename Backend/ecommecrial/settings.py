ab920e8a916fc0b4d13ef908dcbb54dd3c6c859f
dawid <dee2ess@gmail.com>
2021-09-19 11:18:21 +0200

    Backend image upload

--- Backend/ecommecrial/settings.py
+++ Backend/ecommecrial/settings.py
@@ -164,8 +164,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
