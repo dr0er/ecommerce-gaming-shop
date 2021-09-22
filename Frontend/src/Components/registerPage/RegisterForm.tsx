954bfbf5659d5dc46b82a56581fd984044dcbcd2
godspeed7 <pawelpikus7@gmail.com>
2021-09-22 23:42:52 +0200

    rebasing

--- Frontend/src/Components/registerPage/RegisterForm.tsx
+++ Frontend/src/Components/registerPage/RegisterForm.tsx
@@ -1,16 +1,26 @@
-import { useState, MouseEvent } from 'react'
+import { useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import SubmitBtn from './SubmitBtn'
 import { faCheck, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const RegisterForm = () => {
-  const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    passwordConfirm: '',
-    areTermsAccepted: false,
-  })
+  interface IFormInputs {
+    name: string
+    email: string
+    password: string
+    passwordConfirm: string
+    acceptTerms: boolean
+  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    getValues,
+    formState: { errors },
+  } = useForm<IFormInputs>()
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(JSON.stringify(data))
 
   type passwordsVisibility = { pass: boolean; passConfirm: boolean }
   const [passwordsVisibility, setPasswordsVisibility] = useState<passwordsVisibility>({
@@ -18,115 +28,119 @@ const RegisterForm = () => {
     passConfirm: false,
   })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value, type, checked } = e.target
-    setUserData({
-      ...userData,
-      [name]: type === 'checkbox' ? checked : value,
-    })
-  }
-
-  const handleRegister = (e: MouseEvent<HTMLButtonElement>): void => {
-    e.preventDefault()
-    //todo: POST userData to server
-  }
-
   const handleChangePasswordVisibility = (iconId: keyof passwordsVisibility): void => {
     setPasswordsVisibility((prev) => ({ ...passwordsVisibility, [iconId]: !prev[iconId] }))
   }
 
   return (
     <div className="flex flex-col gap-4 px-4 mt-4 w-full">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-2 w-full">
           <label htmlFor="name">Name</label>
           <input
             id="name"
             className={`py-2 px-6 w-full duration-300 rounded-2xl outline-none border-2 
-                        ${
-                          !userData.name && 'opacity-30'
-                        }  border-grey-light focus:opacity-100 bg-background-grey`}
+                        ${!watch('name') && 'opacity-30'}  
+                        border-grey-light focus:opacity-100 bg-background-grey`}
             type="text"
-            value={userData.name}
-            name="name"
             placeholder="Orangutan"
-            onChange={handleChange}
-            required
+            {...register('name', {
+              required: 'This field is required!',
+              maxLength: 20,
+            })}
           />
+          <p className="text-red-500">{errors?.name?.message}</p>
         </div>
         <div className="flex flex-col gap-2 mt-4">
           <label htmlFor="email">Email</label>
           <input
             id="email"
             className={`py-2 px-6 w-full duration-300 rounded-2xl outline-none border-2 
-                        ${
-                          !userData.email && 'opacity-30'
-                        }  border-grey-light focus:opacity-100 bg-background-grey`}
+                        ${!watch('email') && 'opacity-30'}  
+                        border-grey-light focus:opacity-100 bg-background-grey`}
             type="email"
-            value={userData.email}
-            name="email"
             placeholder="mario@gmail.com"
-            onChange={handleChange}
-            required
+            {...register('email', {
+              required: 'This field is required!',
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: 'Email is not valid',
+              },
+            })}
           />
+          <p className="text-red-500">{errors?.email?.message}</p>
         </div>
         <div className="flex flex-col gap-2 mt-4">
           <label htmlFor="password">Password</label>
           <div
-            className={`relative ${!userData.password && 'opacity-30'} focus-within:opacity-100`}>
+            className={`relative ${!watch('password') && 'opacity-30'} focus-within:opacity-100`}>
             <input
               id="password"
               className="py-2 px-6 pr-12 w-full duration-300 rounded-2xl outline-none border-2 
                                 border-grey-light focus:opacity-100 bg-background-grey"
               type={passwordsVisibility.pass ? 'text' : 'password'}
-              value={userData.password}
-              name="password"
               placeholder="password..."
-              onChange={handleChange}
-              required
+              {...register('password', {
+                required: 'Password is required!',
+                minLength: {
+                  value: 6,
+                  message: 'Password has to be at least 6 characters long!',
+                },
+              })}
             />
+
             <FontAwesomeIcon
               icon={passwordsVisibility.pass ? faEye : faEyeSlash}
-              className="absolute flex top-1/2 right-5 transform -translate-y-2/4"
+              className="absolute top-9 right-5 transform -translate-y-5"
               onClick={() => handleChangePasswordVisibility('pass')}
             />
           </div>
+          <p className="text-red-500">{errors?.password?.message}</p>
         </div>
         <div className="flex flex-col gap-2 mt-4 w-full">
           <label htmlFor="passwordConfirm">Repeat password</label>
           <div
-            className={`relative w-full ${
-              !userData.passwordConfirm && 'opacity-30'
-            } focus-within:opacity-100 `}>
+            className={`relative w-full ${!watch('passwordConfirm') && 'opacity-30'} 
+            focus-within:opacity-100 `}>
             <input
               id="passwordConfirm"
               className="py-2 px-6 pr-12 w-full  rounded-2xl outline-none border-2 
                                 border-grey-light focus:opacity-100 bg-background-grey"
               type={passwordsVisibility.passConfirm ? 'text' : 'password'}
-              value={userData.passwordConfirm}
-              name="passwordConfirm"
               placeholder="password..."
-              onChange={handleChange}
-              required
+              {...register('passwordConfirm', {
+                required: 'Password is required!',
+                minLength: {
+                  value: 6,
+                  message: 'Password has to be at least 6 characters long!',
+                },
+                validate: {
+                  matchesPreviousPassword: (value) => {
+                    const { password } = getValues()
+                    return password === value || 'Passwords should match!'
+                  },
+                },
+              })}
             />
+
             <FontAwesomeIcon
               id="passConfirm"
               icon={passwordsVisibility.passConfirm ? faEye : faEyeSlash}
-              className="absolute flex top-1/2 right-5 transform -translate-y-2/4"
+              className="absolute top-9 right-5 transform -translate-y-5"
               onClick={() => handleChangePasswordVisibility('passConfirm')}
             />
           </div>
+          <p className="text-red-500">{errors?.passwordConfirm?.message}</p>
         </div>
         <div className="flex gap-2 mt-4 items-center ">
-          <label htmlFor="areTermsAccepted" className="cursor-pointer relative">
+          <label htmlFor="acceptTerms" className="cursor-pointer relative">
             <input
-              id="areTermsAccepted"
+              id="acceptTerms"
               className="appearance-none w-4 h-4 border-2 rounded-md border-grey-light opacity-30 active:opacity-100"
-              name="areTermsAccepted"
               type="checkbox"
-              checked={userData.areTermsAccepted}
-              onChange={handleChange}
-              required
+              {...register('acceptTerms', {
+                required: 'Please accept Terms and Conditions.',
+              })}
             />
             <FontAwesomeIcon
               icon={faCheck}
@@ -135,8 +149,9 @@ const RegisterForm = () => {
           </label>
           <span>Accept Terms and Conditions</span>
         </div>
+        <p className="text-red-500">{errors?.acceptTerms?.message}</p>
 
-        <SubmitBtn register={handleRegister}>Register</SubmitBtn>
+        <SubmitBtn>Register</SubmitBtn>
       </form>
     </div>
   )
--- Frontend/src/Components/registerPage/SubmitBtn.tsx
+++ Frontend/src/Components/registerPage/SubmitBtn.tsx
@@ -1,17 +1,15 @@
 import React, { FunctionComponent, ReactNode } from 'react'
 
 interface btnProps {
-  register: (e: React.MouseEvent<HTMLButtonElement>) => void
   children: ReactNode
 }
 
-const SubmitBtn: FunctionComponent<btnProps> = ({ register, children }) => {
+const SubmitBtn: FunctionComponent<btnProps> = ({ children }) => {
   return (
     <div>
       <button
         type="submit"
-        className="flex px-9 py-3 w-full justify-center bg-grey-light text-white rounded-2xl text-sm mt-9 cursor-pointer"
-        onSubmit={register}>
+        className="flex px-9 py-3 w-full justify-center bg-grey-light text-white rounded-2xl text-sm mt-9 cursor-pointer">
         {children}
       </button>
     </div>
