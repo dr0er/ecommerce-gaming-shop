3c2334db3ac9d739dc65bca085a953d970607393
godspeed7 <pawelpikus7@gmail.com>
2021-09-23 00:03:18 +0200

    rebase changes

--- Frontend/src/Components/registerPage/BackToMain.tsx
+++ Frontend/src/Components/registerPage/BackToMain.tsx
@@ -1,18 +1,11 @@
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
 import { Routes } from '../../Utilities/routes'
 
 const BackToMain = () => {
   return (
     <Link to={Routes.index}>
-=======
-
-const BackToMain = () => {
-  return (
-    <Link to="/">
->>>>>>> e4fdf3c... minor fixes, added DaisyUI tailwind plugin
       <div className="flex gap-5 items-center text-base font-bold cursor-pointer w-max mb-4">
         <FontAwesomeIcon icon={faChevronLeft} />
         Back to main page
