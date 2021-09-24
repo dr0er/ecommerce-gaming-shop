b91fa1e83e18c5a82ec140d80e68b133495d094a
Wiktor Kedzierawski <victorowskyy@gmail.com>
2021-09-24 16:17:41 +0200

    reack hook form

--- Frontend/src/Components/LoginPage/LoginForm.tsx
+++ Frontend/src/Components/LoginPage/LoginForm.tsx
@@ -2,10 +2,15 @@ import { faCheck, faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface Credentials {
+	email: string;
+	password: string;
+}
 
 const LoginForm = () => {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+	const { register, handleSubmit, watch } = useForm();
 
 	const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
@@ -16,46 +21,46 @@ const LoginForm = () => {
 	};
 
 	// NEED VALIDATION HERE
-	const handleLogin = (e: any) => {
-		e.preventDefault();
-		setEmail("");
-		setPassword("");
-		setIsRememberMe(false);
+	const handleLogin = (e: Credentials) => {
+		const email = e.email;
+		const password = e.password;
+		const data = {
+			email,
+			password,
+			remember: isRememberMe,
+		};
+		console.log(data);
 	};
 
 	return (
 		<div className="flex flex-col gap-4 px-4">
-			<form>
+			<form onSubmit={handleSubmit(handleLogin)}>
 				<div className="flex flex-col gap-2">
 					<p>Email</p>
 					<input
 						type="text"
 						placeholder="Enter your email"
 						className={`py-2 px-6 w-full duration-300 rounded-2xl outline-none border-2 ${
-							!email && "opacity-30"
+							!Boolean(watch("email")) && "opacity-30"
 						}  border-grey-light focus:opacity-100 bg-background-grey`}
-						value={email}
-						onChange={(e) => {
-							setEmail(e.target.value);
-						}}
+						{...register("email")}
 					/>
 				</div>
 
 				<div className="flex flex-col gap-2 mt-2">
 					<p>Password</p>
 					<div
-						className={`relative duration-300 ${
-							!password && "opacity-30"
+						className={`relative ${
+							!Boolean(watch("password")) && "opacity-30"
 						} focus-within:opacity-100 `}
 					>
 						<input
 							type={isPasswordVisible ? "text" : "password"}
 							placeholder="Enter your password"
-							className={`py-2 px-6 w-full  rounded-2xl  outline-none border-2  border-grey-light  bg-background-grey focus:opacity-100`}
-							value={password}
-							onChange={(e) => {
-								setPassword(e.target.value);
-							}}
+							className={`py-2 px-6 w-full duration-300   rounded-2xl  outline-none border-2  border-grey-light  bg-background-grey focus:opacity-100 ${
+								!Boolean(watch("password")) && "opacity-30"
+							}`}
+							{...register("password")}
 						/>
 
 						<FontAwesomeIcon
@@ -65,7 +70,11 @@ const LoginForm = () => {
 						/>
 					</div>
 				</div>
-				<button className="hidden" type="submit" onClick={handleLogin}></button>
+				<button
+					className="hidden"
+					type="submit"
+					onClick={handleSubmit(handleLogin)}
+				></button>
 			</form>
 
 			{/* REMEMBE ME AND RESET PASSWORD */}
@@ -77,7 +86,13 @@ const LoginForm = () => {
 					<div className="border-2 w-5 h-5 rounded-md my-auto flex border-gray-400">
 						{isRememberMe && <FontAwesomeIcon icon={faCheck} />}
 					</div>
-					<p className="opacity-30 font-bold text-sm my-auto">Remember me</p>
+					<p
+						className={`opacity-30 duration-300 font-bold text-sm my-auto ${
+							isRememberMe && "opacity-100"
+						}`}
+					>
+						Remember me
+					</p>
 				</div>
 				<p className="font-extrabold text-sm">Reset password</p>
 			</div>
@@ -85,7 +100,7 @@ const LoginForm = () => {
 			{/* LOGIN BUTTON */}
 			<div
 				className="flex px-9 py-3 justify-center bg-grey-light text-white rounded-2xl text-sm mt-9 font-bold cursor-pointer"
-				onClick={handleLogin}
+				onClick={handleSubmit(handleLogin)}
 			>
 				Login
 			</div>
