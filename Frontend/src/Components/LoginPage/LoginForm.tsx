1ddf6b1e96dba7a050dc959a0495cd5729c4117d
nezouse <stankiewicz1998@gmail.com>
2021-09-24 20:06:45 +0200

    Rewrite login page to react-hook-form

--- Frontend/src/Components/LoginPage/LoginForm.tsx
+++ Frontend/src/Components/LoginPage/LoginForm.tsx
@@ -1,91 +1,86 @@
-import { faCheck, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { MouseEventHandler, useState } from 'react'
+import { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
+
+interface LoginFormData {
+  email?: string
+  password?: string
+  rememberMe?: boolean
+}
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>()
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-
-  const [isRememberMe, setIsRememberMe] = useState(false)
-
   const handleChangePasswordVisibility = () => {
     setIsPasswordVisible((prev) => !prev)
   }
 
-  // NEED VALIDATION HERE
-  const handleLogin: MouseEventHandler<HTMLButtonElement> = (e) => {
-    e.preventDefault()
-    setEmail('')
-    setPassword('')
-    setIsRememberMe(false)
-  }
+  const handleLogin = handleSubmit((data) => {
+    console.log(data)
+  })
 
   return (
-    <div className="flex flex-col gap-4 px-4">
-      <form>
-        <div className="flex flex-col gap-2">
-          <p>Email</p>
+    <form onSubmit={handleLogin} className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          placeholder="Enter your email"
+          className={`py-2 px-6 w-full duration-300 rounded-2xl outline-none border-2 border-gray-400 focus:border-gray-700 bg-background-grey`}
+          {...register('email', { required: 'Email is required.' })}
+        />
+        <ErrorMessage name={'email'} errors={errors} as={<ErrorSpan />} />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="password">Password</label>
+        <div className={`relative duration-300`}>
           <input
-            type="text"
-            placeholder="Enter your email"
-            className={`py-2 px-6 w-full duration-300 rounded-2xl outline-none border-2 ${
-              !email && 'opacity-30'
-            }  border-grey-light focus:opacity-100 bg-background-grey`}
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value)
-            }}
+            id="password"
+            type={isPasswordVisible ? 'text' : 'password'}
+            placeholder="Enter your password"
+            className={`py-2 px-6 w-full rounded-2xl outline-none border-2 border-gray-400 focus:border-gray-700 bg-background-grey focus:opacity-100`}
+            {...register('password', { required: 'Password is required.' })}
           />
-        </div>
 
-        <div className="flex flex-col gap-2 mt-2">
-          <p>Password</p>
-          <div
-            className={`relative duration-300 ${
-              !password && 'opacity-30'
-            } focus-within:opacity-100 `}>
-            <input
-              type={isPasswordVisible ? 'text' : 'password'}
-              placeholder="Enter your password"
-              className={`py-2 px-6 w-full  rounded-2xl  outline-none border-2  border-grey-light  bg-background-grey focus:opacity-100`}
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value)
-              }}
-            />
-
+          <button type="button" onClick={handleChangePasswordVisibility}>
             <FontAwesomeIcon
               icon={isPasswordVisible ? faEyeSlash : faEye}
               className="absolute flex top-1/2 right-7 transform -translate-y-2/4"
-              onClick={handleChangePasswordVisibility}
             />
-          </div>
+          </button>
         </div>
-        <button className="hidden" type="submit" onClick={handleLogin} />
-      </form>
+        <ErrorMessage errors={errors} name="password" as={<ErrorSpan />} />
+      </div>
 
-      {/* REMEMBE ME AND RESET PASSWORD */}
-      <div className="flex justify-between ">
-        <div className="flex gap-3 align-middle" onClick={() => setIsRememberMe((prev) => !prev)}>
-          <div className="border-2 w-5 h-5 rounded-md my-auto flex border-gray-400">
-            {isRememberMe && <FontAwesomeIcon icon={faCheck} />}
-          </div>
-          <p className="opacity-30 font-bold text-sm my-auto">Remember me</p>
+      <div className="flex justify-between">
+        <div className="flex gap-3 align-middle">
+          <input
+            id="rememberMe"
+            type="checkbox"
+            className="h-5 w-5 cursor-pointer"
+            style={{ accentColor: '#f3f5f7' }}
+            {...register('rememberMe')}
+          />
+          <label
+            htmlFor="rememberMe"
+            className="opacity-30 font-bold text-sm my-auto cursor-pointer">
+            Remember me
+          </label>
         </div>
         <p className="font-extrabold text-sm">Reset password</p>
       </div>
 
-      {/* LOGIN BUTTON */}
-      <button
-        className="flex px-9 py-3 justify-center bg-grey-light text-white rounded-2xl text-sm mt-9 font-bold cursor-pointer"
-        onClick={handleLogin}>
+      <button className="flex px-9 py-3 justify-center bg-grey-light text-white rounded-2xl text-sm mt-6 font-bold cursor-pointer">
         Login
       </button>
-
-      {/* SIGN UP */}
 
       <div className="text-grey-light font-bold text-sm flex gap-3 justify-center">
         <span className="opacity-30"> Don&apos;t have an account yet?</span>
@@ -93,8 +88,10 @@ const LoginForm = () => {
           <span className="text-blue-500 opacity-100 cursor-pointer underline">Sign up here!</span>
         </Link>
       </div>
-    </div>
+    </form>
   )
 }
+
+const ErrorSpan: FC = ({ children }) => <p className="text-red-500">{children}</p>
 
 export default LoginForm
