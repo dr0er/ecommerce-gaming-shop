c3bc576925718507bfd07a50bb511a091deca6f7
Wiktor Kedzierawski <victorowskyy@gmail.com>
2021-09-24 17:32:56 +0200

    Route

--- Frontend/src/Components/landingPage/LandingPage.tsx
+++ Frontend/src/Components/landingPage/LandingPage.tsx
@@ -3,27 +3,28 @@ import "./LandingPage.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { Routes } from "../../Utilities/routes";
 
 interface IProps {}
 
 // React.FC<Props> wymaga od komponentu zeby zwracal jsx
 
 export const LandingPage: React.FC<IProps> = (props) => {
-  return (
-    <div className="landingPage_Container">
-      <div className="center_Text">
-        <p className="stragan_Text">Stragan.com</p>
-        <div className="center_Text_Info">
-          Next level of gaming <span className="shopping_Blue">shopping</span>
-        </div>
-      </div>
+	return (
+		<div className="landingPage_Container">
+			<div className="center_Text">
+				<p className="stragan_Text">Stragan.com</p>
+				<div className="center_Text_Info">
+					Next level of gaming <span className="shopping_Blue">shopping</span>
+				</div>
+			</div>
 
-      <div className="button_GetStarted">
-        <Link to="/">
-          Get started
-          <FontAwesomeIcon icon={faChevronRight} />
-        </Link>
-      </div>
-    </div>
-  );
+			<div className="button_GetStarted">
+				<Link to={Routes.mainPage}>
+					Get started
+					<FontAwesomeIcon icon={faChevronRight} />
+				</Link>
+			</div>
+		</div>
+	);
 };
