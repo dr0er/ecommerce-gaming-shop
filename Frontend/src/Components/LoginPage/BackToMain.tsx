5989ab2588baeba21ba2e706803fc506d662c23b
Wiktor Kedzierawski <victorowskyy@gmail.com>
2021-09-24 16:20:01 +0200

    Link to mainPage

--- Frontend/src/Components/LoginPage/BackToMain.tsx
+++ Frontend/src/Components/LoginPage/BackToMain.tsx
@@ -1,10 +1,11 @@
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import { Routes } from "../../Utilities/routes";
 
 const BackToMain = () => {
 	return (
-		<Link to="/">
+		<Link to={Routes.mainPage}>
 			<div className="flex gap-5 items-center text-base font-bold cursor-pointer w-max">
 				<FontAwesomeIcon icon={faChevronLeft} />
 				Back to main page
