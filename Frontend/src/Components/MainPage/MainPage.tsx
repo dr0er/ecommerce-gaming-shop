453be458ef002ba656c3b97ba653fabd17b69f26
godspeed7 <pawelpikus7@gmail.com>
2021-09-26 22:41:00 +0200

    set state for searchbar to show/hide

--- Frontend/src/Components/MainPage/MainPage.tsx
+++ Frontend/src/Components/MainPage/MainPage.tsx
@@ -4,12 +4,15 @@ import { Carousel } from './Carousel'
 import { SpecialCarousel } from './SpecialCarousel'
 
 export const MainPage = () => {
+  const isSearchResult = false // need to agree with backend on the way to fetch search results
+
   return (
     <Layout title="MainPage">
       <div className="mx-auto px-6 w-full">
         <SearchBar />
       </div>
       <div className="flex pl-6 flex-col gap-6">
+        {isSearchResult && <Carousel title="Search results" />}
         <Carousel title="Most popular" />
         <Carousel title="Special offer" />
         <SpecialCarousel />
