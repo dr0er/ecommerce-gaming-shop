81b22c270b9b328f6c0597e953bc4fa8c6ca6e52
godspeed7 <pawelpikus7@gmail.com>
2021-09-26 15:44:27 +0200

    searchbar layout

--- Frontend/src/Components/MainPage/MainPage.tsx
+++ Frontend/src/Components/MainPage/MainPage.tsx
@@ -1,10 +1,14 @@
 import Layout from '../Layout/Layout'
+import SearchBar from '../SearchBar'
 import { Carousel } from './Carousel'
 import { SpecialCarousel } from './SpecialCarousel'
 
 export const MainPage = () => {
   return (
     <Layout title="MainPage">
+      <div className="mx-auto px-6 ">
+        <SearchBar />
+      </div>
       <div className="flex pl-6 flex-col gap-6">
         <Carousel title="Most popular" />
         <Carousel title="Special offer" />
--- Frontend/src/Components/SearchBar.tsx
+++ Frontend/src/Components/SearchBar.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+
 const SearchBar = () => {
-  return <div>Heloo world!</div>
+  const itemList = ['mouse', 'keyboard', 'monitor']
+  const [search, setSearch] = useState('')
+
+  const filterItems =
+    search.length === 0
+      ? itemList
+      : itemList.filter((item) => item.toLowerCase().includes(search.toLowerCase()))
+
+  return (
+    <>
+      <input
+        value={search}
+        type="text"
+        name="search"
+        placeholder="search..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div>
+        {filterItems.map((item) => (
+          <p key={filterItems.indexOf(item)}>{item}</p>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default SearchBar
