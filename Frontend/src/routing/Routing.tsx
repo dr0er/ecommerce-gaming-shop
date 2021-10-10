62a4fe47ae63b4617f6994d43aff05d7589e34ca
tomaszciula <tomciuns@gmail.com>
2021-10-10 08:14:14 +0200

    user panel with orders

--- Frontend/src/routing/Routing.tsx
+++ Frontend/src/routing/Routing.tsx
@@ -8,6 +8,7 @@ import { MainPage } from '../Components/MainPage/MainPage'
 import LoginPage from '../Components/LoginPage/LoginPage'
 import RegisterPage from '../Components/registerPage/RegisterPage'
 import OrderItemWrapper from '../Components/OrderListItem/OrderItemWrapper'
+import UserPanel from '../Components/UserPanel/UserPanel'
 
 export const Routing = () => {
   return (
@@ -35,8 +36,13 @@ export const Routing = () => {
       <Route path={Routes.signUp}>
         <RegisterPage />
       </Route>
+
       <Route path="/orderItem">
         <OrderItemWrapper />
+      </Route>
+
+      <Route path="/userPanel">
+        <UserPanel />
       </Route>
     </Switch>
   )
