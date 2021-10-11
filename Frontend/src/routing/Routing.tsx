3ce85be0f3aa53474bf87e80d332fac7639dc11f
godspeed7 <pawelpikus7@gmail.com>
2021-10-11 21:13:28 +0200

    routing

--- Frontend/src/routing/Routing.tsx
+++ Frontend/src/routing/Routing.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Routes } from '../Utilities/routes'
 import { LandingPage } from '../Components/landingPage/LandingPage'
