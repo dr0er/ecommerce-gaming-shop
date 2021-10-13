7dc6784912e048cba1ca7f7a985513de8074504e
Wiktor Kedzierawski <victorowskyy@gmail.com>
2021-10-13 10:06:17 +0200

    cursor pointer, use shared component

--- Frontend/src/Components/LoginPage/LoginPage.tsx
+++ Frontend/src/Components/LoginPage/LoginPage.tsx
@@ -1,16 +1,16 @@
-import BackToMain from "./BackToMain";
-import LoginForm from "./LoginForm";
+import BackToMain from '../shared/BackToMain'
+import LoginForm from './LoginForm'
 
 const LoginPage = () => {
-	return (
-		<div className="h-screen text-grey-light py-6 flex flex-col gap-8 align-middle  max-w-md px-6 md: mx-auto">
-			<BackToMain />
+  return (
+    <div className="h-screen text-grey-light py-6 flex flex-col gap-8 align-middle  max-w-md px-6 md: mx-auto">
+      <BackToMain />
 
-			<div className="font-bold text-3xl text-center ">Login to Stragan</div>
+      <div className="font-bold text-3xl text-center ">Login to Stragan</div>
 
-			<LoginForm />
-		</div>
-	);
-};
+      <LoginForm />
+    </div>
+  )
+}
 
-export default LoginPage;
+export default LoginPage
--- Frontend/src/Components/shared/Checkbox.tsx
+++ Frontend/src/Components/shared/Checkbox.tsx
@@ -1,30 +1,28 @@
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface CheckboxProps {
-	setCheckbox: React.Dispatch<React.SetStateAction<boolean>>;
-	isCheckbox: Boolean;
-	text: string;
+  setCheckbox: React.Dispatch<React.SetStateAction<boolean>>
+  isCheckbox: boolean
+  text: string
 }
 
 const Checkbox = ({ setCheckbox, isCheckbox, text }: CheckboxProps) => {
-	return (
-		<div
-			className="flex gap-3 align-middle"
-			onClick={() => setCheckbox((prev: any) => !prev)}
-		>
-			<div className="border-2 w-5 h-5 rounded-md my-auto flex border-gray-400">
-				{isCheckbox && <FontAwesomeIcon icon={faCheck} />}
-			</div>
-			<p
-				className={`opacity-30 duration-300 font-bold text-sm my-auto ${
-					isCheckbox && "opacity-100"
-				}`}
-			>
-				{text}
-			</p>
-		</div>
-	);
-};
+  return (
+    <div
+      className="flex gap-3 align-middle cursor-pointer"
+      onClick={() => setCheckbox((prev: any) => !prev)}>
+      <div className="border-2 w-5 h-5 rounded-md my-auto flex border-gray-400">
+        {isCheckbox && <FontAwesomeIcon icon={faCheck} />}
+      </div>
+      <p
+        className={`opacity-30 duration-300 font-bold text-sm my-auto ${
+          isCheckbox && 'opacity-100'
+        }`}>
+        {text}
+      </p>
+    </div>
+  )
+}
 
-export default Checkbox;
+export default Checkbox
